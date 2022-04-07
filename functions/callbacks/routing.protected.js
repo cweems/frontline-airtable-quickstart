3395d0d5ec3f09372f6aa83b5b6ea915361d1fbc
Charlie Weems <charlie.weems@gmail.com>
2022-04-06 17:44:01 -0700

    Lint routing callback

--- functions/callbacks/routing.protected.js
+++ functions/callbacks/routing.protected.js
@@ -1,50 +1,50 @@
-const path = Runtime.getAssets()['/providers/customers.js'].path;
-const { findWorkerForCustomer, findRandomWorker } = require(path);
+// eslint-disable-next-line no-undef
+const path = Runtime.getAssets()['/providers/customers.js'].path
+const { findWorkerForCustomer, findRandomWorker } = require(path)
 
-exports.handler = async function(context, event, callback) {
-    console.log("Handling custom routing callback.");
-    
-    let client = context.getTwilioClient();
+exports.handler = async function (context, event, callback) {
+  console.log('Handling custom routing callback.')
 
-    try {
-        const conversationSid = event.ConversationSid;
-        const customerNumber = event['MessagingBinding.Address'];
-    
-        const workerIdentity = await routeConversation(context, conversationSid, customerNumber);
-        const resp = await routeConversationToWorker(client, conversationSid, workerIdentity);
+  const client = context.getTwilioClient()
 
-        callback(null, resp);
-    } catch(err) {
-        callback(err);
-    }
-};
+  try {
+    const conversationSid = event.ConversationSid
+    const customerNumber = event['MessagingBinding.Address']
+
+    const workerIdentity = await routeConversation(context, conversationSid, customerNumber)
+    const resp = await routeConversationToWorker(client, conversationSid, workerIdentity)
+
+    callback(null, resp)
+  } catch (err) {
+    callback(err)
+  }
+}
 
 const routeConversation = async (context, conversationSid, customerNumber) => {
-    let workerIdentity = await findWorkerForCustomer(context, customerNumber);
+  let workerIdentity = await findWorkerForCustomer(context, customerNumber)
 
-    if (!workerIdentity) { // Customer doesn't have a worker
+  if (!workerIdentity) { // Customer doesn't have a worker
+    // Select a random worker
+    console.log('No assigned worker found, selecting a random worker.')
+    workerIdentity = await findRandomWorker(context)
 
-        // Select a random worker
-        console.log('No assigned worker found, selecting a random worker.')
-        workerIdentity = await findRandomWorker(context);
+    // Or you can define default worker for unknown customers.
+    // workerIdentity = 'john@example.com'
 
-        // Or you can define default worker for unknown customers.
-        // workerIdentity = 'john@example.com'
-
-        if (!workerIdentity) {
-            throw new Error(`Routing failed, please add workers to customersToWorkersMap or define a default worker. Conversation SID: ${conversationSid}`)
-        }
+    if (!workerIdentity) {
+      throw new Error(`Routing failed, please add workers to customersToWorkersMap or define a default worker. Conversation SID: ${conversationSid}`)
     }
+  }
 
-    return workerIdentity;    
+  return workerIdentity
 }
 
 const routeConversationToWorker = async (client, conversationSid, workerIdentity) => {
-    // Add worker to the conversation with a customer
-    await client.conversations
-        .conversations(conversationSid)
-        .participants
-        .create({ identity: workerIdentity })
-        .then(participant => console.log('Created agent participant: ', participant.sid))
-        .catch(err => console.log(`Failed to create agent participant: ${err}`));
+  // Add worker to the conversation with a customer
+  await client.conversations
+    .conversations(conversationSid)
+    .participants
+    .create({ identity: workerIdentity })
+    .then(participant => console.log('Created agent participant: ', participant.sid))
+    .catch(err => console.log(`Failed to create agent participant: ${err}`))
 }
